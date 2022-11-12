0ecc5b2c808deab4adc8dff1603053650e08eb53
neeramrutia <105576636+neeramrutia@users.noreply.github.com>
2022-11-12 18:01:23 +0530

    Time: 48 ms (6.21%), Space: 17.2 MB (10.17%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int currmax=prices[0];
-        int currmin=prices[0];
-        int ans=0;
-        for(int i=1;i<prices.size();i++)
-        {
-            if(prices[i]>currmax)
-            {
-                currmax=prices[i];
-            }
-            else
-            {
-                ans+=currmax-currmin;
-                currmax=prices[i];
-                currmin=prices[i];
-            }
+         vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return dfs(prices, 0, 1,dp);
+    }
+     int dfs(vector<int> &p, int i, bool buy, vector<vector<int>> &dp){
+        int n = p.size();
+        if(i == n) return 0;
+        
+        if(dp[i][buy] != -1) return dp[i][buy];
+        
+        int ans = 0;
+        if(buy){
+            int b = -p[i] + dfs(p, i+1, 0, dp);
+            int nb = 0 + dfs(p, i+1, 1, dp);
+            ans = max(b, nb);
         }
-        ans=ans+currmax-currmin;
-        return ans;
+        else{
+            int s = p[i] + dfs(p, i+1, 1, dp);
+            int ns = 0 + dfs(p, i+1, 0, dp);
+            ans = max(s, ns);
+        }
+        return dp[i][buy] = ans;
+        
     }
 };
